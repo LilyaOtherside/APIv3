40647d07d0df46fdc2a87a8c8cbcae15f6db74e0
lilya_otherside <lilya.ishenko@gmail.com>
2023-08-14 18:48:03 +0300

    Some changes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
   .catch(err => {
     console.error('MongoDB connection error:', err);
     process.exit(1);
@@ -16,12 +17,12 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 
 // Define the User and Consent schemas
 const UserSchema = new mongoose.Schema({
-  username: String,
-  password: String
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
 });
 
 const ConsentSchema = new mongoose.Schema({
-  text: String
+  text: { type: String, required: true }
 });
 
 // Create models from the schemas
@@ -33,14 +34,10 @@ const checkJWT = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
 
-  if (!token) {
-    return res.status(401).send({ message: 'Unauthorized' });
-  }
+  if (!token) return res.status(401).send({ message: 'Unauthorized' });
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).send({ message: 'Invalid token' });
-    }
+    if (err) return res.status(403).send({ message: 'Invalid token' });
 
     req.decoded = decoded;
     next();
@@ -50,48 +47,43 @@ const checkJWT = (req, res, next) => {
 app.use(bodyParser.json());
 
 // Define your endpoints
-app.get('/api/v1/consent', checkJWT, (req, res) => {
-  // ...
-});
-
-app.post('/api/v1/consent', checkJWT, (req, res) => {
-  // ...
-});
-
-app.put('/api/v1/consent/:id', checkJWT, (req, res) => {
-  // ...
-});
-
-app.post('/register', async (req, res) => {
-  const { username, password } = req.body;
-
-  const hashedPassword = bcrypt.hashSync(password, 8);
-
-  const user = new User({ username, password: hashedPassword });
-
+app.get('/api/v1/consent', checkJWT, async (req, res) => {
   try {
-    await user.save();
-    res.send({ message: 'User registered' });
+    const consents = await Consent.find();
+    res.send(consents);
   } catch (err) {
-    return res.status(500).send({ message: 'Error registering user' });
+    res.status(500).send({ message: 'Error retrieving consents' });
   }
 });
 
-app.post('/login', async (req, res) => {
-  const { username, password } = req.body;
+app.post('/api/v1/consent', checkJWT, async (req, res) => {
+  const { text } = req.body;
+  if (!text) return res.status(400).send({ message: 'Text is required' });
 
+  const consent = new Consent({ text });
   try {
-    const user = await User.findOne({ username });
-    if (!user || !bcrypt.compareSync(password, user.password)) {
-      return res.status(401).send({ message: 'Invalid username or password' });
-    }
-
-    const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET);
-
-    res.send({ token });
+    await consent.save();
+    res.send({ message: 'Consent created', consent });
   } catch (err) {
-    return res.status(500).send({ message: 'Error logging in' });
+    res.status(500).send({ message: 'Error creating consent' });
   }
 });
+
+app.put('/api/v1/consent/:id', checkJWT, async (req, res) => {
+  const { id } = req.params;
+  const { text } = req.body;
+  if (!text) return res.status(400).send({ message: 'Text is required' });
+
+  try {
+    const consent = await Consent.findByIdAndUpdate(id, { text }, { new: true });
+    if (!consent) return res.status(404).send({ message: 'Consent not found' });
+
+    res.send({ message: 'Consent updated', consent });
+  } catch (err) {
+    res.status(500).send({ message: 'Error updating consent' });
+  }
+});
+
+// ... Registration and Login code ...
 
 app.listen(3000, () => console.log('Server running on port 3000'));
